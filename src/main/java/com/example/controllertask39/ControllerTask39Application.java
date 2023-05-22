d19254a07cc5d52863da3e8ea475d8247ff9b6c1
Pavlo Moskovchuk <Pavlo.moskovchukgmail.com>
2023-05-22 10:18:43 +0300

    Initial commit

---
+++ src/main/java/com/example/controllertask39/ControllerTask39Application.java
@@ -0,0 +1,13 @@
+package com.example.controllertask39;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerTask39Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerTask39Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/controllertask39/ControllerTask39ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controllertask39;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerTask39ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
